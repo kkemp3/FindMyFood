6f51399d254176598c499ee588c40693d53700c7
Kevin <kkemp3@wisc.edu>
2020-08-23 12:50:08 -0500

    Created XCode project

---
+++ FindMyFood/FindMyFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindMyFood
+//
+//  Created by Kevin Kemp on 8/23/20.
+//  Copyright © 2020 Kevin Kemp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
